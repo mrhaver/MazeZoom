806e42eaa3eae0b5b57eb64b1b4ca9ba825e1e97
Koen Vestjens <koenvestjens@gmail.com>
2017-03-28 14:07:39 +0200

    artifact model added

---
+++ MazeZoom.Core.Profiling/Models/Artifact.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeZoom.Core.Profiling.Models
+{
+    class Artifact
+    {
+        private string name;
+        private DateTime date;
+        private string url;
+
+        #region getters & setters
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public DateTime getDate()
+        {
+            return this.date;
+        }
+
+        public void setDate(DateTime date)
+        {
+            this.date = date;
+        }
+
+        public string getUrl()
+        {
+            return this.url;
+        }
+
+        public void setUrl(string url)
+        {
+            this.url = url;
+        }
+
+        #endregion
+
+        public Artifact()
+        {
+
+        }
+
+        
+    }
+}
