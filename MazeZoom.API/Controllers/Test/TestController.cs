9fb3a81cd48b17e59fc5f88982d78c569ca8322b
Koen Vestjens <koenvestjens@gmail.com>
2017-04-04 16:00:22 +0200

    post request return value

--- MazeZoom.API/Controllers/Test/TestController.cs
+++ MazeZoom.API/Controllers/Test/TestController.cs
@@ -1,5 +1,7 @@
 ﻿using MazeZoom.Core.Profiling;
 using MazeZoom.Core.Profiling.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,9 +17,9 @@ namespace MazeZoom.API.Controllers.Test
 {
     public class TestController : ApiController
     {
-        // GET: api/Test
-        [Route("api/core/profiling/getartifacts")]
-        public HttpResponseMessage Get()
+
+        [Route("api/core/profiling/getall")]
+        public HttpResponseMessage GetArtifacts()
         {
             Profiler p = new Profiler();
             TempMemoryCollection tmc = new TempMemoryCollection();
@@ -28,30 +30,61 @@ namespace MazeZoom.API.Controllers.Test
             return response;
         }
 
-        [Route("api/core/profiling/valuate")]
-        [HttpGet, HttpPost]
-        public HttpResponseMessage ValuateArtifact(string artifactId)
+        [Route("api/core/profiling/getallresults")]
+        public HttpResponseMessage GetAllResults()
         {
+            Profiler p = new Profiler();
+            TempMemoryCollection tmc = new TempMemoryCollection();
+            IEnumerable<Artifact> artifacts = tmc.Artifacts;
 
-            HttpResponseMessage response = Request.CreateResponse<string>(HttpStatusCode.OK, artifactId);
-            response.Headers.Add("Access-Control-Origin-Allow", "*");
+            HttpResponseMessage response = Request.CreateResponse<IEnumerable<Artifact>>(HttpStatusCode.OK, artifacts);
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
             return response;
+        }
+
+        /*[Route("api/core/profiling/postjudge")]
+        [HttpGet, HttpPost]
+        public HttpResponseMessage Post([FromBody]string token)
+        {
+            HttpResponseMessage response = Request.CreateResponse<string>(HttpStatusCode.OK, token);
+            HttpContent requestContent = Request.Content;
+            string jsonContent = requestContent.ReadAsStringAsync().Result;
+
+
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return response;
+        }*/
+
+        [Route("api/core/profiling/postjudge")]
+        [HttpPost]
+        public String Post([FromBody]dynamic value)
+        {
+            //var x = value.artifacts; // JToken
+
+            //String jsonString = Convert.ToString(value.artifacts);
+            //dynamic results = JsonConvert.DeserializeObject<Artifact>(jsonString);
+
+            /*String jsonString = Convert.ToString(value.artifacts);
+         
+            JArray array = JArray.Parse(jsonString);
+            foreach (JObject obj in array.Children<JObject>())
+            {
+                int id = Convert.ToInt32(obj["id"]);
+                String name = obj["name"].ToString();
+                DateTime date = Convert.ToDateTime(obj["date"].ToString());
+                String url = obj["url"].ToString();
+                Judgement judgement = (Judgement)Convert.ToInt32(obj["judgement"]);
+
+                new Artifact(id, name, date, url, judgement);
+            }*/
+
+            return Convert.ToString(value);
         }
 
         // GET: api/Profiling/5
         public IHttpActionResult Get(int id)
         {
             return Ok("value");
-        }
-
-        [Route("api/core/profiling/post")]
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-            if(value != null)
-            {
-                
-            }
         }
 
         // PUT: api/Profiling/5
@@ -63,5 +96,15 @@ namespace MazeZoom.API.Controllers.Test
         public void Delete(int id)
         {
         }
+
+/*
+     [{
+	"id": 1,
+	"name": "test",
+	"date": "01/08/2008 14:50:50.42",
+	"url": "http://www.google.nl",
+	"judgement": "Judgement.NONE"
+    }]
+*/
     }
 }
